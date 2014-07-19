eb29edfa3cb5ff653e6f271cac7f19e8e9108680
Chris Strom <github@eeecomputes.com>
2014-07-18 23:31:16 -0400

    Helpful comments in main benchmarking script

--- visitor/tool/benchmark.sh
+++ visitor/tool/benchmark.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Initialize artifact directory
 mkdir -p tmp
 cat /dev/null > tmp/benchmark_loop_runs.tsv
 cat /dev/null > tmp/benchmark_summary.tsv
 
+# Individual benchmark runs of different implementations
 echo "Running benchmarks..."
 for X in 10 100 1000 10000 100000
 do
@@ -13,11 +15,11 @@ do
 done
 echo "Done. Results stored in tmp/benchmark_loop_runs.tsv."
 
+# Summarize results
 echo "Building summary..."
-
 ./tool/summarize_results.dart
-
 echo "Done. Results stored in tmp/benchmark_summary.tsv."
 
+# Visualization ready
 echo ""
 echo "To view in gnuplot, run tool/visualize.sh."
