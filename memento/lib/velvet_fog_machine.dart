5c660b471c394d7d143d10ebf575a460fbf2b583
Chris Strom <github@eeecomputes.com>
2015-11-30 00:25:35 -0500

    Improve memento example w/ more realistic originator structure

--- memento/lib/velvet_fog_machine.dart
+++ memento/lib/velvet_fog_machine.dart
@@ -1,35 +1,48 @@
 library velvet_fog_machine;
 
 import 'dart:math';
+final rand = new Random(1);
 
 // The "Originator"
 class VelvetFogMachine {
-  // The Memento
-  Playing _nowPlaying;
+  Song currentSong;
+  double currentTime;
 
   // Set the state
   void play(String title, String album, [double time = 0.0]) {
-    print("Playing $title // $album @ ${time.toStringAsFixed(2)}");
-    _nowPlaying = new Playing(title, album, time);
+    _play(new Song(title, album), time);
   }
 
+  void _play(Song s, double t) {
+    currentSong = s;
+    currentTime = t;
+    print("Playing $currentSong @ ${currentTime.toStringAsFixed(2)}");
+  }
+
+  // Create a memento of the current state
   Playing get nowPlaying {
-    _nowPlaying.time = 5*rand.nextDouble();
-    return _nowPlaying;
+    // Simulate playing at some time later:
+    var time = 5*rand.nextDouble();
+
+    return new Playing(currentSong, time);
   }
 
   // Restore from memento
-  void backTo(Playing memento) {
+  void backTo(Playing p) {
     print("  *** Whoa! This was a good one, let's hear it again :) ***");
-    play(memento.title, memento.album, memento.time);
+    _play(p.song, p.time);
   }
 }
 
-final rand = new Random(1);
+class Song {
+  String title, album;
+  Song(this.title, this.album);
+  String toString() => "$title // $album";
+}
 
 // The Memento
 class Playing {
-  String title, album;
+  Song song;
   double time;
-  Playing(this.title, this.album, this.time);
+  Playing(this.song, this.time);
 }
