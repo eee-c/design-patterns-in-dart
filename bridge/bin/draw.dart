4958af2cb8e1e00f268458fbcb9f80908677a249
Chris Strom <github@eeecomputes.com>
2016-01-27 01:02:50 -0500

    First pass at bridge pattern

---
+++ bridge/bin/draw.dart
@@ -0,0 +1,67 @@
+#!/usr/bin/env dart
+
+// Implementor
+abstract class DrawingApi {
+  void drawCircle(double x, double y, double radius);
+}
+
+// Concrete Implementor 1
+class DrawingApi1 implements DrawingApi {
+  void drawCircle(double x, double y, double radius) {
+    print(
+      "[DrawingApi1] "
+      "circle at ($x, $y) with "
+      "radius ${radius.toStringAsFixed(3)}"
+    );
+  }
+}
+
+// Concrete Implementor 2
+class DrawingApi2 implements DrawingApi {
+  void drawCircle(double x, double y, double radius) {
+    print(
+      "[DrawingApi2] "
+      "circle at ($x, $y) with "
+      "radius ${radius.toStringAsFixed(3)}"
+    );
+  }
+}
+
+// Abstraction
+abstract class Shape {
+  DrawingApi _drawingApi;
+
+  Shape(this._drawingApi);
+
+  void draw();                         // low-level
+  void resizeByPercentage(double pct); // high-level
+}
+
+// Refined Abstraction
+class Circle extends Shape {
+  double _x, _y, _radius;
+  Circle(this._x, this._y, this._radius, DrawingApi api) :
+    super(api);
+
+  // low-level i.e. Implementation specific
+  void draw() {
+    _drawingApi.drawCircle(_x, _y, _radius);
+  }
+  // high-level i.e. Abstraction specific
+  void resizeByPercentage(double pct) {
+    _radius *= (1.0 + pct/100.0);
+  }
+}
+
+// Client
+main() {
+  List<Shape> shapes = [
+    new Circle(1.0, 2.0, 3.0, new DrawingApi1()),
+    new Circle(5.0, 7.0, 11.0, new DrawingApi2())
+  ];
+
+  shapes.forEach((shape){
+    shape.resizeByPercentage(2.5);
+    shape.draw();
+  });
+}
