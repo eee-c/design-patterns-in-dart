eca0bff1929f7ed2ae864a1512349898af89d5f9
Chris Strom <github@eeecomputes.com>
2016-01-30 13:23:01 -0500

    Revert "Silly handle-body experiment for sharing implementors"
    One too many late night hacking sesssions :-\
    
    This reverts commit a5c6ca2da2cb3f08ed2a108309eac2988af58643.

--- bridge/bin/draw.dart
+++ bridge/bin/draw.dart
@@ -1,21 +1,18 @@
 #!/usr/bin/env dart
 
+import 'dart:mirrors' show reflectClass;
+
 // Implementor
 abstract class DrawingApi {
-  int _refCount = 0;
-  void ref() {
-    _refCount++;
-    print('  $this Increased refcount to $_refCount');
-  }
-  void deref() {
-    _refCount--;
-    print('  $this Decreased refcount to $_refCount');
-  }
   void drawCircle(double x, double y, double radius);
 }
 
 // Concrete Implementor 1
-class DrawingApi1 extends DrawingApi {
+class DrawingApi1 implements DrawingApi {
+  static final DrawingApi1 _drawingApi = new DrawingApi1._internal();
+  factory DrawingApi1()=> _drawingApi;
+  DrawingApi1._internal();
+
   void drawCircle(double x, double y, double radius) {
     print(
       "[DrawingApi1] "
@@ -26,7 +23,7 @@ class DrawingApi1 extends DrawingApi {
 }
 
 // Concrete Implementor 2
-class DrawingApi2 extends DrawingApi {
+class DrawingApi2 implements DrawingApi {
   void drawCircle(double x, double y, double radius) {
     print(
       "[DrawingApi2] "
@@ -37,33 +34,31 @@ class DrawingApi2 extends DrawingApi {
 }
 
 // Abstraction
-
 abstract class Shape {
   DrawingApi _drawingApi;
+  static Map<Type, DrawingApi> _drawingApiCache = {};
 
-  set drawer(DrawingApi other) {
-    other.ref();
-    if (_drawingApi != null) {
-      _drawingApi.deref();
-      if (_drawingApi._refCount == 0) {
-        print('  ** Deleting no longer used $_drawingApi **');
-        _drawingApi = null;
-      }
-    }
-    _drawingApi = other;
+  Shape(Type drawingApi) {
+    _drawingApi = _drawingApiCache.putIfAbsent(
+      drawingApi,
+      ()=> reflectClass(drawingApi).newInstance(new Symbol(''), []).reflectee
+    );
   }
 
+  void reset() { _drawingApiCache.clear(); }
+
+  void draw();                         // low-level
   void resizeByPercentage(double pct); // high-level
 }
 
 // Refined Abstraction
 class Circle extends Shape {
   double _x, _y, _radius;
-  Circle(this._x, this._y, this._radius);
+  Circle(this._x, this._y, this._radius, Type drawingApi) :
+    super(drawingApi);
 
   // low-level i.e. Implementation specific
   void draw() {
-    if (_drawingApi == null) return;
     _drawingApi.drawCircle(_x, _y, _radius);
   }
   // high-level i.e. Abstraction specific
@@ -74,23 +69,18 @@ class Circle extends Shape {
 
 // Client
 main() {
-  var api1 = new DrawingApi1(),
-      api2 = new DrawingApi2();
+  List<Shape> shapes = [
+    new Circle(1.0, 2.0, 3.0, DrawingApi1),
+    new Circle(0.0, 6.0, 1.0, DrawingApi1),
+    new Circle(2.0, 2.0, 1.5, DrawingApi1),
+    new Circle(5.0, 7.0, 11.0, DrawingApi2),
+    new Circle(1.0, 2.0, 3.0, DrawingApi1),
+    new Circle(5.0, -7.0, 1.0, DrawingApi2),
+    new Circle(-1.0, -2.0, 5.0, DrawingApi1)
+  ];
 
-  var circle1 = new Circle(1.0, 2.0, 3.0)..drawer = api1,
-      circle2 = new Circle(0.0, 6.0, 1.0)..drawer = api1,
-      circle3 = new Circle(2.0, 2.0, 1.5)..drawer = api2;
-
-  circle1.draw();
-  circle2.draw();
-  circle3.draw();
-
-  circle1.drawer = api2;
-  circle2.drawer = api2;
-
-  api1 = api2 = null;
-
-  circle1.draw();
-  circle2.draw();
-  circle3.draw();
+  shapes.forEach((shape){
+    shape.resizeByPercentage(2.5);
+    shape.draw();
+  });
 }
